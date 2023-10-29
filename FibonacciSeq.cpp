80853cd3aed76aa48873d1cf87ca95c76ebd0f6d
Mustafa Dohad <mustafamurtazadohad@gmail.com>
2023-10-29 15:24:37 -0400

    Add file via upload

---
+++ FibonacciSeq.cpp
@@ -0,0 +1,22 @@
+// Print the fibonacci sequence //
+#include<iostream>
+using namespace std;
+
+void Fibo (int num){
+    int a = 0, b = 1, c ;
+    for (int i = 1; i <= num; i++)
+    {
+        cout<<a<<endl;
+        c = a+b;
+        a = b;
+        b = c;
+    }
+    return ;
+}
+int main()
+{
+    int t1=0,t2=0,n;
+    cin>>n;
+    Fibo(n);
+    return 0; 
+}git
