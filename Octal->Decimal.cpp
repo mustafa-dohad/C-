c94f1fa6af5ec29799ad694ba5d215f99bb7ddc8
Mustafa Dohad <mustafamurtazadohad@gmail.com>
2023-10-30 03:08:10 -0400

    Correction

--- Octal->Decimal.cpp
+++ Octal->Decimal.cpp
@@ -5,7 +5,7 @@
 #include <cmath>
 using namespace std;
 
-int Convert(long long num)
+int Convert(int num)
 {
     int r, total = 0, i = 0;
     while (num != 0)
@@ -21,7 +21,7 @@ int Convert(long long num)
 
 int main()
 {
-    long long n;
+    int n;
     cin >> n;
     cout << n << " in octal = " << Convert(n) << " in decimal";
     return 0;
